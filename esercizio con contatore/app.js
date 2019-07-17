64fc51b541e77a4a0fd5853efc7aec6fd723d9aa
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-17 16:42:16 +0200

    aggiunti commenti

--- esercizio con contatore/app.js
+++ esercizio con contatore/app.js
@@ -8,16 +8,24 @@ $(document).ready(function () {
 
   $(".square").on("click", function () {
 
+    //controllo se i quadrati sono stati già premuti
     if ($(this).hasClass("clicked") === false) {
+
+      //controllo se i quadrati hanno la classe red, se no li coloro di verde
       if ($(this).hasClass("red")) {
         $(this).css("background-color", "red");
+
         redCounter += 1;
         redLabel.innerHTML = redCounter;
+
+        //aggiungo la classe clicked, per impedire di aumentare il contatore sullo stesso quadrato
         $(this).addClass("clicked");
       } else {
         $(this).css("background-color", "green");
         greenCounter += 1;
         greenLabel.innerHTML = greenCounter;
+        
+        //aggiungo la classe clicked, per impedire di aumentare il contatore sullo stesso quadrato
         $(this).addClass("clicked");
       }
     }
