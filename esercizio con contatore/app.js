05c221baab9bebbb8725ca81e4ded3377f059def
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-18 10:02:05 +0200

    formattazione

--- esercizio con contatore/app.js
+++ esercizio con contatore/app.js
@@ -13,21 +13,23 @@ $(document).ready(function () {
 
       //controllo se i quadrati hanno la classe red, se no li coloro di verde
       if ($(this).hasClass("red")) {
+
         $(this).css("background-color", "red");
 
         redCounter += 1;
         redLabel.innerHTML = redCounter;
 
-        //aggiungo la classe clicked, per impedire di aumentare il contatore sullo stesso quadrato
-        $(this).addClass("clicked");
       } else {
+
         $(this).css("background-color", "green");
+
         greenCounter += 1;
         greenLabel.innerHTML = greenCounter;
         
-        //aggiungo la classe clicked, per impedire di aumentare il contatore sullo stesso quadrato
-        $(this).addClass("clicked");
+        
       }
+      //aggiungo la classe clicked, per impedire di aumentare il contatore sullo stesso quadrato
+        $(this).addClass("clicked");
     }
   })
 
